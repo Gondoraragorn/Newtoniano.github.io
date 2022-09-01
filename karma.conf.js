7f987bcc487fb1a8336fc488592c1b59282be67d
Gondoraragorn <Joel.garcia1202@hotmail.com>
2022-09-01 19:35:14 +0200

    b0.2

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Website'),
+      dir: require('path').join(__dirname, './coverage/WebsiteNew'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import { ProjectsComponent } from './projects/projects.component';
 import { SocialMediaComponent } from './social-media/social-media.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { NotfoundComponent } from './notfound/notfound.component';
-import { CodingComponent } from './coding/coding.component';
-import { ResumeComponent } from './resume/resume.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    ContactComponent,
-    ProjectsComponent,
     SocialMediaComponent,
-    NavbarComponent,
-    NotfoundComponent,
-    CodingComponent,
-    ResumeComponent
+    HomeComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
